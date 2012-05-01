ba89490dc45a766852da5d0252985f36b031cd84
AaronTufts <aarontietz@gmail.com>
2012-04-30 22:33:07 -0400

    added new files I fogot to add

---
+++ Staff/ChordInstrumentsController.h
@@ -0,0 +1,25 @@
+//
+//  InstrumentsController.h
+//  Staff
+//
+//  Created by Christopher Harris on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+@interface ChordInstrumentsController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource>
+{
+    NSArray *elements;
+    UIView *content;
+    int instrument;
+    IBOutlet UIPickerView *picker;
+}
+
+@property (nonatomic, retain) IBOutlet UIPickerView *picker;
+@property int instrument;
+
+- (void)setupInstrumentsElements;
+
+@end
